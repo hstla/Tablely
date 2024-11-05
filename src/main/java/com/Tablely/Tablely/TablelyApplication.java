d7f3ecdbf7a3f7235e12e4be8a4108e84292567d
hstla <ree7243@gmail.com>
2024-11-06 00:30:13 +0900

    프로젝트 시작

---
+++ src/main/java/com/Tablely/Tablely/TablelyApplication.java
@@ -0,0 +1,13 @@
+package com.Tablely.Tablely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablelyApplication.class, args);
+	}
+
+}
