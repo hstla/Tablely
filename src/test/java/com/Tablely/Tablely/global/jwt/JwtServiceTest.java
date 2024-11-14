b84ca2a6ab61b83099aa4e3f9f7df06cfdfa1b15
hhwang <hhwang@magicsoft.co.kr>
2024-11-14 17:55:01 +0900

    [feature]: 토큰 테스트 변경[#1]

--- src/test/java/com/Tablely/Tablely/global/jwt/JwtServiceTest.java
+++ src/test/java/com/Tablely/Tablely/global/jwt/JwtServiceTest.java
@@ -1,42 +1,50 @@
 package com.Tablely.Tablely.global.jwt;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import com.Tablely.Tablely.user.domain.UserType;
-import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * 어노테이션 설명
- * ExtendWith:
- * MockitoExtension.class:
+ * @ExtendWith(MockitoExtension.class): mockito를 사용할 수 있도록 하는 어노테이션 @Mock, @InjectMocks 사용 가능
+ * @InjectMocks: mock 객체들을 실제 객체에 자동으로 주입하는 어노테이션이다.
  */
-
 @ExtendWith(MockitoExtension.class)
 class JwtServiceTest {
 
-    @MockBean
-    private JwtService jwtService = new JwtService();
+    @InjectMocks
+    private JwtService jwtService;
 
+    @DisplayName("토큰을 생성하고 그 토큰을 통해 사용자 정보를 가져온다.")
     @Test
-    void 토큰_정상_동작() {
+    void createToken() {
         //given
         JwtUserInfo jwtUserInfo = new JwtUserInfo(1L, UserType.CUSTOMER);
         String token = jwtService.createToken(jwtUserInfo);
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        when(request.getHeader("Authorization")).thenReturn(token);
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
         //when
-        JwtUserInfo result = jwtService.getUserId();
+        JwtUserInfo userInfo = jwtService.getUserInfo(token);
+
         //then
-        assertThat(result.getUserId()).isEqualTo(jwtUserInfo.getUserId());
-        assertThat(result.getUserType()).isEqualTo(jwtUserInfo.getUserType());
+        assertThat(userInfo.getUserId()).isEqualTo(jwtUserInfo.getUserId());
+        assertThat(userInfo.getUserType()).isEqualTo(jwtUserInfo.getUserType());
+    }
+
+    @DisplayName("토큰을 생성하고 검증한다.")
+    @Test
+    void validateToken() {
+        //given
+        JwtUserInfo jwtUserInfo = new JwtUserInfo(1L, UserType.CUSTOMER);
+        String token = jwtService.createToken(jwtUserInfo);
+
+        //when
+        boolean validateToken = jwtService.validateToken(token);
+
+        //then
+        assertThat(validateToken).isTrue();
     }
 }
